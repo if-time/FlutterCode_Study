8e0ae3d337cb25bafd15f4a84d83931f05bb52df
if-time <if-time@users.noreply.github.com>
2019-02-27 20:28:11 +0800

    2

--- flutter_app1/lib/main.dart
+++ flutter_app1/lib/main.dart
@@ -1,42 +1,63 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  final String title;
+  final String desc;
+
+  Product(this.title, this.desc);
+}
+
 void main() {
   runApp(MaterialApp(
-    title: "导航",
-    home: new FirstScreen(),
+    title: '导航',
+    home: ProductList(
+        products: List.generate(20, (i) => Product('商品 $i', '这是商品详情 : $i'))),
   ));
 }
 
-class FirstScreen extends StatelessWidget {
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+  ProductList({Key key, @required this.products}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('导航页面')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('查看'),
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) => new SecondScreen()));
-          },
-        ),
+      appBar: AppBar(
+        title: Text('商品列表'),
+      ),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(products[index].title),
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) => ProductDetail(product: products[index])
+                )
+                );
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
+
+class ProductDetail extends StatelessWidget {
+
+  final Product product;
+
+  ProductDetail({Key key, @required this.product}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('详情')),
+      appBar: AppBar(title: Text('商品详情${product.title}'),),
       body: Center(
-        child: RaisedButton(
-          child: Text('返回'),
-          onPressed: (){
-            Navigator.pop(context);
-          },
-        ),
+        child: Text('${product.desc}'),
       ),
     );
   }
